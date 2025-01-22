05962013a104fa30a67cc6ff050a8f4ae93603de
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-01-22 15:17:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,32 @@
 
+# Configure Azure Provider
 terraform {
-  required_version = ">=0.12"
-
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~>4.0"
-    }
+      source = "hashicorp/azurerm"
+      version = ">= 3.59.0"
+    } 
   }
+  required_version = ">= 0.14.9"
 }
 
 provider "azurerm" {
   features {}
 
-skip_provider_registration = "true"
+  skip_provider_registration = "true"
   
   # Connection to Azure
   subscription_id = var.subscription_id
   client_id = var.client_id
   client_secret = var.client_secret
   tenant_id = var.tenant_id
-  }
+}
+
+variable "prefix" {
+  default = "terraform"
+}
 
 resource "azurerm_resource_group" "rg10" {
+  name     = "resource_group_10"
   location = "Central India"
-  name     = "rg10"
 }
